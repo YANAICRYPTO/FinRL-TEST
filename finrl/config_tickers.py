0b7fd828c248cc9571a6eb53a97eb4b8b5e33c3b
YANAICRYPTO <149383565+YANAICRYPTO@users.noreply.github.com>
2024-01-28 23:46:45 +0000

    Update config_tickers.py

--- finrl/config_tickers.py
+++ finrl/config_tickers.py
@@ -4,37 +4,7 @@ SINGLE_TICKER = ["AAPL"]
 
 # Dow 30 constituents in 2021/10
 # check https://wrds-www.wharton.upenn.edu/ for U.S. index constituents
-DOW_30_TICKER = [
-    "AXP",
-    "AMGN",
-    "AAPL",
-    "BA",
-    "CAT",
-    "CSCO",
-    "CVX",
-    "GS",
-    "HD",
-    "HON",
-    "IBM",
-    "INTC",
-    "JNJ",
-    "KO",
-    "JPM",
-    "MCD",
-    "MMM",
-    "MRK",
-    "MSFT",
-    "NKE",
-    "PG",
-    "TRV",
-    "UNH",
-    "CRM",
-    "VZ",
-    "V",
-    "WBA",
-    "WMT",
-    "DIS",
-    "DOW",
+DOW_30_TICKER = ['NVDA'
 ]
 
 # Nasdaq 100 constituents at 2019/01
